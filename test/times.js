328e3f5a26ae6ed4ca2110386b270e1de115f21f
Sergey Kozlov <dartess@mail.ru>
2023-08-07 16:11:25 +0400

    times

--- test/times.js
+++ test/times.js
@@ -1,20 +1,21 @@
 var assert = require('assert');
 
-var R = require('../source/index.js');
+var R = require('remeda');
 var eq = require('./shared/eq.js');
 
 
 describe('times', function() {
   it('takes a map func', function() {
-    eq(R.times(R.identity, 5), [0, 1, 2, 3, 4]);
-    eq(R.times(function(x) {
+    eq(R.times(5, R.identity), [0, 1, 2, 3, 4]);
+    eq(R.times(5, function(x) {
       return x * 2;
-    }, 5), [0, 2, 4, 6, 8]);
+    },), [0, 2, 4, 6, 8]);
   });
 
   it('throws if second argument is not a valid array length', function() {
-    assert.throws(function() { R.times(3)('cheers!'); }, RangeError);
-    assert.throws(function() { R.times(R.identity, -1); }, RangeError);
+    // NOTE не применимо на уровне типов
+    // assert.throws(function() { R.times(3)('cheers!'); }, RangeError);
+    assert.throws(function() { R.times(-1, R.identity); }, RangeError);
   });
 
 });
