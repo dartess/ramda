d197f67fcbf30f31bcee7142d07c3f2f8ba4875d
Sergey Kozlov <dartess@mail.ru>
2023-08-07 15:28:06 +0400

    omit

--- test/omit.js
+++ test/omit.js
@@ -1,4 +1,4 @@
-var R = require('../source/index.js');
+var R = require('remeda');
 var eq = require('./shared/eq.js');
 
 
@@ -6,15 +6,16 @@ describe('omit', function() {
   var obj = {a: 1, b: 2, c: 3, d: 4, e: 5, f: 6};
 
   it('copies an object omitting the listed properties', function() {
-    eq(R.omit(['a', 'c', 'f'], obj), {b: 2, d: 4, e: 5});
+    eq(R.omit(obj, ['a', 'c', 'f']), {b: 2, d: 4, e: 5});
   });
 
-  it('includes prototype properties', function() {
-    var F = function(param) {this.x = param;};
-    F.prototype.y = 40; F.prototype.z = 50;
-    var obj = new F(30);
-    obj.v = 10; obj.w = 20;
-    eq(R.omit(['w', 'x', 'y'], obj), {v: 10, z: 50});
-  });
+  // NOTE не применимо
+  // it('includes prototype properties', function() {
+  //   var F = function(param) {this.x = param;};
+  //   F.prototype.y = 40; F.prototype.z = 50;
+  //   var obj = new F(30);
+  //   obj.v = 10; obj.w = 20;
+  //   eq(R.omit(obj, ['w', 'x', 'y']), {v: 10, z: 50});
+  // });
 
 });
